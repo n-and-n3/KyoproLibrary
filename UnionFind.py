cccc3cb80343008c1e4e43ab5ab066cc5016a7df
n3 <alphagameplayerpro@gmail.com>
2025-10-10 11:10:38 +0900

    UnionFindを拡張

--- UnionFind.py
+++ UnionFind.py
@@ -5,11 +5,13 @@
 class UnionFind:
   def __init__(self,N):
     self.parent = [-1]*N
+    self.groupcount = N
 
   def union(self,x,y):
     fx = self.find(x)
     fy = self.find(y)
     if fx != fy: # x,y の属する木が異なる
+      self.count -= 1
       if self.parent[fx] <= self.parent[fy]: # xの木のサイズ >= yの木のサイズ
         self.parent[fx] += self.parent[fy]
         self.parent[fy] = fx
@@ -30,3 +32,17 @@ class UnionFind:
 
   def size(self,x):
     return -self.parent[self.find(x)]
+
+  def same(self,x,y):
+    return self.find(x) == self.find(y)
+
+  def groups(self):
+    A = sorted(set(self.find(i) for i in range(len(self.parent))))
+    d = {v:i for i,v in enumerate(A)}
+    res = [[] for i in range(len(A))]
+    for i in range(len(self.parent)):
+      res[d[self.find(i)]].append(i)
+    return res
+
+  def groupsizes(self):
+    return [len(g) for g in self.groups()]
