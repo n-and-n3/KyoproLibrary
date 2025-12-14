84f79594fbde493fd353a18e1734d6f1cc1c298b
n&(n//3)==0 <135620637+n-and-n3@users.noreply.github.com>
2025-12-14 17:01:47 +0900

    Fix syntax error in SegmentTree write method

--- Python/SegmentTree.py
+++ Python/SegmentTree.py
@@ -21,7 +21,7 @@ class SegmentTree:  # 0-indexed
     ii = i + self.N
     self.array[ii] = x
     while ii > 1:
-      self.array[ii>>1] = self.func(self.array[ii&0],self.array[ii|1])
+      self.array[ii>>1] = self.func(self.array[ii&(-2)(,self.array[ii|1])
       ii >>= 1
     
   def reduce(self,l,r): # 半開区間[l,r)でのfunc
